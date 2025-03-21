11f658a600281d1e0506e5970e63c28e491371f6
Reka <r_thanabalan@yahoo.ca>
2025-03-21 08:16:03 -0400

    created login component

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
-import SignUpForm from "./components/SignUpForm";
+import React, { useState } from "react";
+import SignUpForm from "./components/SignUpForm";  
+import Login from "./components/Login";          
 
-function App() {
+const App = () => {
+  const [isSignUp, setIsSignUp] = useState(true);
+
+  // Function to toggle between SignUp and Login
+  const toggleForm = () => {
+    setIsSignUp((prev) => !prev);
+  };
+
   return (
     <div>
-      <SignUpForm />
+      {isSignUp ? <SignUpForm toggleForm={toggleForm} /> : <Login toggleForm={toggleForm} />}
     </div>
   );
-}
+};
 
 export default App;
