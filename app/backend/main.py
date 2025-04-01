5b8081408c3139498c5bdeb06f32de19424468c5
wolecodes <opeyemioyeyemi141@gmail.com>
2025-04-01 23:09:58 +0100

    feat: add route cors origin setup

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
-from app.backend.api.routes import auth
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
+from fastapi import FastAPI
 
-app = FastAPI()
+from starlette.middleware.cors import CORSMiddleware
+
+from app.backend.api.main import api_router
+from app.backend.core.config import settings
+
+app = FastAPI(title=settings.PROJECT_NAME)
 
 # Path to the frontend build folder
 frontend_path = Path(__file__).parent.parent / "frontend" / "build"
@@ -16,4 +20,13 @@ if frontend_path.exists():
 def health_check():
     return {"status": "API is running"} 
 
-app.include_router(auth.router)
+# # Set all CORS enabled origins
+# if settings.all_cors_origins:
+#     app.add_middleware(
+#         CORSMiddleware,
+#         allow_origins=settings.all_cors_origins,
+#         allow_credentials=True,
+#         allow_methods=["*"],
+#         allow_headers=["*"],
+#     )
+app.include_router(api_router, prefix=settings.API_V1)
