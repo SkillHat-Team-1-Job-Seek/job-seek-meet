e6a69aa5c1dfa6c3fbe9e222126efccc1d2fa96f
wolecodes <opeyemioyeyemi141@gmail.com>
2025-04-01 15:11:33 +0100

    Fix: return data of the user create crud to correctly pass the actual data

--- app/backend/crud.py
+++ app/backend/crud.py
@@ -4,10 +4,13 @@ from db.models import User
 from sqlalchemy.orm import Session
 
 def create_user(*, session: Session, user_create: UserCreate) -> UserPublic:
-    db_obj = User.model_validate(
-        user_create, update={"hashed_password": get_password_hash(user_create.password)}
+    db_obj = User(
+        first_name=user_create.first_name,
+        email=str(user_create.email),
+        password=get_password_hash(user_create.password)  # Hash the password
     )
     session.add(db_obj)
     session.commit()
     session.refresh(db_obj)
-    return db_obj
+    return UserPublic.model_validate(db_obj)
+
