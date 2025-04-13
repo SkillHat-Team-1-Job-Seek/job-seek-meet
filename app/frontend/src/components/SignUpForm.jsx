a300dd03fb824b9e3cf59b664d061c23326e93b1
Reka <r_thanabalan@yahoo.ca>
2025-04-12 21:57:33 -0400

    updated signUpForm component to test dynamic backend URLs

--- app/frontend/src/components/SignUpForm.jsx
+++ app/frontend/src/components/SignUpForm.jsx
@@ -2,7 +2,9 @@
 import React, { useState } from "react";
 
 // const API_URL = "https://job-seek-meet.onrender.com/";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "https://job-seek-meet.onrender.com";
+
 
 
 const SignUpForm = ({ toggleForm }) => {
