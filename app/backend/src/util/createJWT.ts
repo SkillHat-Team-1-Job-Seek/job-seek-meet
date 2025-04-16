93693ef1327b0eba9b3ea16b015cf3e9f1f5f779
wolecodes <opeyemioyeyemi141@gmail.com>
2025-04-16 19:43:11 +0100

    fix: update .gitignore and clean up unused imports; modify JWT expiration in createJWT

--- app/backend/src/util/createJWT.ts
+++ app/backend/src/util/createJWT.ts
@@ -3,7 +3,7 @@ import { JWT_SECRET, JWT_EXPIRES_IN } from "./secrets";
 
 export const createJWT = (payload: { userID: string }) => {
   const accessToken = jwt.sign(payload, JWT_SECRET!, {
-    expiresIn: JWT_EXPIRES_IN,
+    expiresIn: "14h",
   });
   return accessToken;
 };
