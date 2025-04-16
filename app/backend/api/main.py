a7e2decd9128c279b2eb5d222f02a40efc3ce201
wolecodes <opeyemioyeyemi141@gmail.com>
2025-04-16 02:52:48 +0100

    Remove file

--- app/backend/api/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.backend.api.routes import auth, user
-
-
-api_router = APIRouter()
-api_router.include_router(auth.router)
-api_router.include_router(user.router)
-
-
