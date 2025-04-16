b274b9ef0ce982c8de1e0f7ecf15e19c63acc0af
wolecodes <opeyemioyeyemi141@gmail.com>
2025-04-16 14:45:50 +0100

    feat: add email verification functionality with nodemailer

--- app/backend/src/util/secrets.ts
+++ app/backend/src/util/secrets.ts
@@ -5,3 +5,6 @@ export const JWT_SECRET = `${process.env.JWT_SECRET}`;
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 export const NODE_ENV = process.env.NODE_ENV;
 export const environment = process.env.NODE_ENV;
+export const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS;
+
+export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
