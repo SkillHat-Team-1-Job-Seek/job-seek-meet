952e0c40095aa1890125a25ee2ae020054c73ae4
wolecodes <opeyemioyeyemi141@gmail.com>
2025-04-18 03:06:49 +0100

    fix: set default PORT value in secrets.ts

--- app/backend/src/util/secrets.ts
+++ app/backend/src/util/secrets.ts
@@ -1,6 +1,6 @@
 require("dotenv").config({ path: "./.env" });
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 3001;
 export const JWT_SECRET = `${process.env.JWT_SECRET}`;
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 export const NODE_ENV = process.env.NODE_ENV;
