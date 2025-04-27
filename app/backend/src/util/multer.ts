dfb9e7582388f5eacc540ff01d75d5fe598f5c23
wolecodes <opeyemioyeyemi141@gmail.com>
2025-04-27 04:30:13 +0100

    feat: add Cloudinary and Multer integration for image uploads; update Prisma schema with new user and connection models

---
+++ app/backend/src/util/multer.ts
@@ -0,0 +1,15 @@
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+import cloudinary from "./cloudinary";
+
+//configure storage
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "jobBuddies_image",
+    allowed_formats: ["jpg", "jpeg", "png"],
+    transformation: [{ width: 500, height: 500, crop: "limit" }],
+  } as any,
+});
+
+export const upload = multer({ storage });
